b4ebc36fd5683bf968f546c03fe6c253943f0294
Alok Sarmah Bordoloi <106934852+AlokSarmahBordoloi@users.noreply.github.com>
2022-08-19 00:33:14 +0530

    difference between dot and breaket notation

---
+++ code/objects/f34.js
@@ -0,0 +1,13 @@
+// difference between dot and breaket notation
+
+const key = "email";
+const person = {
+    name: "alok",
+    age: 20,
+   "person hobbies": ["guitar" ,"sleeping", "listening music"]
+}
+
+// console.log(person["person hobbies"]);
+
+person[key] = "aloksarmah70001@gmail.com";
+console.log(person);
