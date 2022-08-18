c74c5d3ca0adc87f1b98bda3d964a4c15629fe65
Alok Sarmah Bordoloi <106934852+AlokSarmahBordoloi@users.noreply.github.com>
2022-08-19 00:30:27 +0530

    for in loop

---
+++ code/Arrays/f31.js
@@ -0,0 +1,11 @@
+// for in loop in array 
+const fruits = ["apple", "mango", "grapes"];
+
+for (let index in fruits) {
+    console.log(fruits[index]);
+}
+
+/* syntax:
+    for (key in object) {
+        // code block to be executed
+    }   */
