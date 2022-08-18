48542df9842a7552693cd522b147a8ac0888776e
Alok Sarmah Bordoloi <106934852+AlokSarmahBordoloi@users.noreply.github.com>
2022-08-19 00:28:49 +0530

    while loop

---
+++ code/Arrays/f29.js
@@ -0,0 +1,12 @@
+//while loop   in array
+
+const fruits = ["apple", "mango"]
+const fruits2 = [];
+
+let i = 0;
+while(i<fruits.length) {
+    // console.log(fruits[i]);
+    fruits2.push(fruits[i])
+    i++;
+}
+console.log(fruits2);
