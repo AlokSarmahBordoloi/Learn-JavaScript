b759b3fafe7396bfcaf858281ecdcf9fe4c49b86
Alok Sarmah Bordoloi <106934852+AlokSarmahBordoloi@users.noreply.github.com>
2022-08-19 00:37:06 +0530

    objects inside array

---
+++ code/objects/f39.js
@@ -0,0 +1,16 @@
+//objects inside array
+//very useful real world applications
+
+
+const users = [
+    { userId: 1 , firstName: "Alok" , age: 20},
+    { userId: 2 , firstName: "Harshit" , age: 30},
+    { userId: 3 , firstName: "Harray" , age: 20},
+]
+
+for( let user of users){
+    console.log(user);
+    // console.log(user.firstName);
+}
+
+console.log(users);
