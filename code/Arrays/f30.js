29672d807874ce46ac445065bb50f79d686e2d1c
Alok Sarmah Bordoloi <106934852+AlokSarmahBordoloi@users.noreply.github.com>
2022-08-19 00:29:28 +0530

    for of loop

---
+++ code/Arrays/f30.js
@@ -0,0 +1,12 @@
+// for of loop in array 
+const fruits = ["apple", "mango","grapes"];
+
+for(let fruit of fruits) {
+    console.log(fruits);
+}
+
+/*      for (variable of iterable) {
+            // code block to be executed
+        }
+variable - For every iteration the value of the next property is assigned to the variable. Variable can be declared with const, let, or var.
+iterable - An object that has iterable properties.        */
