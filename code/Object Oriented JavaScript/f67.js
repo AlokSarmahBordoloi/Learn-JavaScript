229339a6f65ea3f20b7924b98c1efa4318f3165f
Alok Sarmah Bordoloi <106934852+AlokSarmahBordoloi@users.noreply.github.com>
2022-08-20 20:56:08 +0530

    create your own  Method

--- code/Object Oriented JavaScript/f67.js
+++ code/Object Oriented JavaScript/f67.js
@@ -1,1 +1,44 @@
+// Method 
+// function inside object    called method 
 
+
+// example1:
+const person ={
+    fName : "Alok",
+    age : 20,
+    about: function(){
+        // console.log(`person name is ${this.fName} and person age is ${this.age}`);
+        console.log(this);
+    }
+} 
+
+person.about();
+
+
+
+
+//  example2:
+
+function personInfo(){
+    console.log(`person name is ${this.fName} and person age is ${this.age}`);
+}
+
+const person1 ={
+    fName : "Alok",
+    age : 20,
+    about: personInfo
+} 
+const person2 ={
+    fName : "Mohit",
+    age : 23,
+    about: personInfo
+} 
+const person3 ={
+    fName : "Harsh",
+    age : 45,
+    about: personInfo
+} 
+
+person1.about();
+person2.about();
+person3.about();
