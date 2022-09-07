abf202d9a05b708cf15c82d38b0e218253ca573f
Alok Sarmah Bordoloi <106934852+AlokSarmahBordoloi@users.noreply.github.com>
2022-09-07 07:04:09 +0530

    getElementById method

---
+++ code/DOM/file2.js
@@ -0,0 +1,12 @@
+// getElementById
+// select element using get elememt by id 
+
+//The getElementById() method returns an element with a specified value.
+
+
+console.log( typeof document.getElementById("main-heading")); //output: object
+
+console.dir(document.getElementById("main-heading")); 
+
+const mainHeading = document.getElementById("main-heading");
+console.log(mainHeading);
