19743ea35fbba559bad429a4af4fa279689f1547
Alok Sarmah Bordoloi <106934852+AlokSarmahBordoloi@users.noreply.github.com>
2022-09-07 07:03:03 +0530

    DOM

---
+++ code/DOM/file1.js
@@ -0,0 +1,7 @@
+//// DOM (Document Object Model
+
+//  The Document Object Model (DOM) is a programming interface for web documents. It represents the page so that programs can change the document structure, style, and content.
+
+console.dir(window.document);
+
+console.dir(document);
