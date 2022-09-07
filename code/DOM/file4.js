436b1b0aebfb4931466f563fa480560e3ff41a4a
Alok Sarmah Bordoloi <106934852+AlokSarmahBordoloi@users.noreply.github.com>
2022-09-07 07:25:47 +0530

    textContent and innerText

---
+++ code/DOM/file4.js
@@ -0,0 +1,22 @@
+// change text 
+// textContent and innerText
+
+// textContent :
+// The textContent property sets or returns the text content of the specified node, and all its descendants.
+
+const mainheading = document.getElementById("main-heading");
+console.log(mainheading.textContent);
+// mainheading.textContent = "This is something wrong";
+// console.log(mainheading.textContent);
+
+
+// innerText :
+// The innerText property sets or returns the text content of an element.
+
+const mainheading2 = document.getElementById("main-heading");
+console.log(mainheading2.innerText);
+
+
+
+// textContent : a html file t lihika hokolu text return koribo.
+// innerText : a webpage t dakibo pua text kheni ha return koribo .
