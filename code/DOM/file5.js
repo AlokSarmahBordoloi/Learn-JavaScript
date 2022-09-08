d8cd6ab494804850bfd2d4980951e903e6c2cdc5
Alok Sarmah Bordoloi <106934852+AlokSarmahBordoloi@users.noreply.github.com>
2022-09-08 12:15:52 +0530

    change the styles of elements

---
+++ code/DOM/file5.js
@@ -0,0 +1,18 @@
+// change the styles of elements
+
+// const mainHeading = document.querySelector("#main-heading");
+const mainHeading = document.querySelector("div.headline h2");
+// console.log(mainHeading.style);
+
+mainHeading.style.color = "blue";   // add color 
+
+mainHeading.style.backgroundColor = "blue";   // add color 
+mainHeading.style.border = "20px solid green"
+
+
+
+/* 
+   To change the style of an HTML element,
+   use this syntax :
+    document.getElementById(id).style.property = new style
+*/
