f27e844b726da1fcccf188dab7b4c153a2b1f878
Alok Sarmah <106934852+AlokSarmahBordoloi@users.noreply.github.com>
2022-10-11 20:36:21 +0530

    Create person.js

---
+++ code/JavaScript Modules/utils/person.js
@@ -0,0 +1,41 @@
+export default class Person{
+    constructor(fName,lName,age){
+        this.fName = fName
+        this.lName = lName
+        this.age = age
+    }
+
+    info(){
+        console.log(this.fName, this.lName, this.age)
+    }
+}
+
+export class Person2{
+    constructor(fName,lName,age){
+        this.fName = fName
+        this.lName = lName
+        this.age = age
+    }
+
+    info(){
+        console.log(this.fName, this.lName, this.age)
+    }
+}
+export class Person3{
+    constructor(fName,lName,age){
+        this.fName = fName
+        this.lName = lName
+        this.age = age
+    }
+
+    info(){
+        console.log(this.fName, this.lName, this.age)
+    }
+}
+
+
+const hello = "Hello World"
+export default hello
+
+// export default can us only 1 time in a single file 
+// export default use kora karna import person r holoni balag likhibo u pari
